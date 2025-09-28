2bd4ba5afc9140e172b90b58ecd44f5e121e020e
Aybars <arslanaybars@gmail.com>
2025-09-28 09:52:19 +0300

    refactor: extract configuration and benchmark setup into helper methods
    - Moved configuration loading into BuildConfiguration()
    - Moved benchmark configuration into BuildBenchmarkConfig()
    - Improved readability and separation of concerns in Program.cs

--- CoreRepo.Benchmark/Program.cs
+++ CoreRepo.Benchmark/Program.cs
@@ -1,16 +1,20 @@
-﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 using CoreRepo.Benchmark;
 using Microsoft.Extensions.Configuration;
 
-Console.WriteLine("Hello, World!");
-var a  = new ConfigurationBuilder()
-    .SetBasePath(AppContext.BaseDirectory)
-    .AddJsonFile("appsettings.Development.json", true, true)
-    .Build();
+static IConfiguration BuildConfiguration() =>
+    new ConfigurationBuilder()
+        .SetBasePath(AppContext.BaseDirectory)
+        .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+        .Build();
 
-var b = a.GetConnectionString("CoreDB");
+static IConfig BuildBenchmarkConfig() =>
+    DefaultConfig.Instance.WithOptions(ConfigOptions.DisableOptimizationsValidator);
 
-var config = DefaultConfig.Instance
-    .WithOptions(ConfigOptions.DisableOptimizationsValidator);
-var summary = BenchmarkRunner.Run<ControllerBenchmark>(config);
+var configuration = BuildConfiguration();
+var connectionString = configuration.GetConnectionString("CoreDB");
+
+Console.WriteLine($"Connection: {connectionString}");
+
+var summary = BenchmarkRunner.Run<ControllerBenchmark>(BuildBenchmarkConfig());
